d538141e7c481877e2f80422eef4c3126d4d822f
UrgingGamer <90156029+UrgingGamer@users.noreply.github.com>
2021-09-07 18:53:24 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Tackling Dependencie for Military Job (Made By: UrgingGamer)'
+
+version '1.0.0'
+
+server_scripts {
+	'server.lua',
+}
+
+client_scripts {
+	'client.lua'
+}
